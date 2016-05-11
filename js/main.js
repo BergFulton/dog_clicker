26ddaba319b2e2e1f683cd230717bc3114cfcdc5
BergFulton <BergFulton@users.noreply.github.com>
2016-05-11 08:08:53 -0400

    WIP: adding second dog image, dog name, additional clicker

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ $( document ).ready(function() {
   		clicks++;
     $("#click_text").html(clicks+" times.");
   });
+
+$("#pics").append("<p>This thing here.</p>");
+$("#pics").prepend("<p>Dog Name</p>");
+
+
 });
+

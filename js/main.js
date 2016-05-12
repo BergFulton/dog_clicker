a084fa598fa479a1ad4ae7e2a28d8ba3497a1cc2
BergFulton <BergFulton@users.noreply.github.com>
2016-05-12 19:43:01 -0400

    wip click counter not iterating properly

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ $( document ).ready(function() {
 	var clicks = 0;
 	$(".dog").click(function() {
   		clicks++;
-    $("#click_text").html(clicks+" times.");
-  });
+    	$(this).next().find(".click_text").html(clicks + " times.");
+  	});
 
-$("#pics").append("<p>This thing here.</p>");
-$("#pics").prepend("<p>Dog Name</p>");
+// $("#pics").append("<p>This thing here.</p>");
+// $("#pics").prepend("<p>Dog Name</p>");
 
 
 });

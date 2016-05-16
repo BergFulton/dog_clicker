3b8607f5ada0501ee32a3b5f7a757b4bf526d2e7
BergFulton <BergFulton@users.noreply.github.com>
2016-05-16 19:04:53 -0400

    wip working on moving all DOM builds to JS

--- js/main.js
+++ js/main.js
@@ -1,20 +1,48 @@
-$( document ).ready(function() {
-	var clicks = 0;
-	$(".dog").click(function() {
-  		clicks++;
-    	$(this).next().find(".click_text").html(clicks + " times.");
-  	});
+$(document).ready(function() {
 
-var dogStats = {
-    'dogs': [{
-        'name': 'Blorps',
-        'img': 'img/basset_hound.jpg',
-        'count': '0',
-        'id': '1',
-  }, {
-  		'name': 'Ralph',
-        'img': 'img/two_basset_hounds.jpg',
-        'count': '0',
-        'id': '2',
-  });
+	var dogStuff = {
+		dogs: [{
+			'name': 'Walter',
+			'imgPath': 'img/basset_hound.jpg'
+		},
+		{
+			'name': 'Shadow',
+			'imgPath': 'img/two_basset_hounds.jpg'
+		}
+		]
+	}
 
+
+	var $dogNameEl = $('.dog-name');
+	for (var i = 0, len = dogStuff.dogs.length; i < len; i++) {
+		$dogNameEl.text(dogStuff.dogs[i].name);
+
+		var $myDogContainer = $('<div class="dog-container" />');
+		var $myDogName = $('<h3 class="dog-name">' + dogStuff.dogs[i].name + '</h3>');
+		$myDogContainer.insertBefore('footer');
+		$myDogName.appendTo('.dog-container');
+	}
+
+	// //Building JS to append images to the div
+	// var HTMLdogPics = '<img src="%data%" class="dogpic">';
+	// var display = function() {
+	// 	for (var i = 0, len = dogStuff.dogs.length; i < len; i++) {
+	// 		var formattedDogPic = HTMLdogPics.replace('%data%', dogStuff.dogs[i].imgPath);
+ //        	$('#dog-container').append(formattedDogPic);
+ //        }
+ //    };
+ //    display();
+
+
+
+    // var increment = function() {
+    // 	var $clickTextEl = $(this).next().find(".click_text");
+    //     $clickTextEl.text(function(i, oldval) {
+    //         return ++oldval;
+    //     });
+    // }
+
+    // $(".dog").on('click', increment);
+});
+
+//for (var i = 0, len = work.jobs.length; i < len; i++) {

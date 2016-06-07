68e8ae8b8afa158e46065e356e8510d217be698d
BergFulton <BergFulton@users.noreply.github.com>
2016-06-06 21:08:28 -0400

    wip: building out the admin view.

--- js/main.js
+++ js/main.js
@@ -1,83 +1,106 @@
 $(document).ready(function() {
 
-/* Model*/
-	var dogStuff = {
-		dogs: [{
-			'name': 'Ralph',
-			'imgPath': 'img/basset_hound.jpg',
-			'clickCount': 0
-		},
-		{
-			'name': 'Blorps & Co.',
-			'imgPath': 'img/two_basset_hounds.jpg',
-			'clickCount': 0
+    /* Model*/
+    var model = {
+    	currentDog: null,
+        dogs: [{
+            'name': 'Ralph',
+            'imgPath': 'img/basset_hound.jpg',
+            'clickCount': 0
+        }, {
+            'name': 'Blorps & Co.',
+            'imgPath': 'img/two_basset_hounds.jpg',
+            'clickCount': 0
 
-		},
-		{
-			'name': 'Walter',
-			'imgPath': 'img/walt_couch.jpg',
-			'clickCount': 0
-		},
-		{
-			'name': 'Shadow',
-			'imgPath': 'img/shadow.jpg',
-			'clickCount': 0
-		},
-		{
-			'name': 'Buddy',
-			'imgPath': 'img/buddy.jpg',
-			'clickCount': 0
-		}
-		]
-	}
+        }, {
+            'name': 'Walter',
+            'imgPath': 'img/walt_couch.jpg',
+            'clickCount': 0
+        }, {
+            'name': 'Shadow',
+            'imgPath': 'img/shadow.jpg',
+            'clickCount': 0
+        }, {
+            'name': 'Buddy',
+            'imgPath': 'img/buddy.jpg',
+            'clickCount': 0
+        }]
+    };
 
-	// for each of the dogs, build their containers & set up their markup
-	$.each(dogStuff.dogs, function(i, dog) {
-		var $dogContainer = $('<div class="dog-container" />');
-		var $dogName = $('<h3 class="dog-name" />');
-		var $dogUL = $('.dog-ul');
-		var $dogNameItem = $('<li class="dog-name-item" />');
-		var $dogImage = $('<img class="dog" />');
-		// var $toggleWrapper = $('<div class="toggle-wrapper" />');
-		var $clickContainer = $('<div class="clicks" />');
-		var $clickText = $('<p>You have clicked the dog:</p>');
-		var $clickCounterContainer = $('<p class="click-text" />');
-		var $dogView = $('<div class ="dog-view" />')
-		
-		// put the markup in the right place on the page
-		$dogContainer.appendTo('.dog-containers-group');
-		$dogName.text(dog.name).appendTo($dogNameItem);
-		// $toggleWrapper.appendTo($dogNameItem);
-		// $dogImage.attr('src', dog.imgPath).appendTo($toggleWrapper);
-		$dogNameItem.appendTo($dogUL);
+    var octopus = {
+    	init: function() {
+    		model.currentDog = model.dogs[0];
+    		dogListView.init();
+    		dogView.init();
+    	},
+    	getCurrentDog: function() {
+    		return model.currentDog;
+    	},
+    	getDogs: function() {
+    		return model.dogs;
+    	},
+    	setCurrentDog: function(dog) {
+    		model.currentDog = dog;
+    	},
+    	incrementCounter: function() {
+    		model.currentDog.clickCount++;
+    		dogView.render();
+    	}
+    };
 
+    var dogView = {
+    	init: function() {
+    		this.dogElem = $('.dog');
+    		this.dogNameElem = $('.dog-name');
+    		this.dogImageElem = $('.dog-img');
+    		this.dogCountElem = $('.click-count');
+
+    		this.dogImageElem.on('click', function(e) {
+    			octopus.incrementCounter();
+    		});
+    		this.render();
+    	},
+    	render: function() {
+    		var currentDog = octopus.getCurrentDog();
+    		this.dogCountElem.text(currentDog.clickCount);
+    		this.dogNameElem.text(currentDog.name);
+    		this.dogImageElem.attr('src', currentDog.imgPath);
+    	}
+    };
+
+    var dogListView = {
+    	init: function() {
+    		this.dogListElem = $('.dog-list');
+    		this.render();
+    	},
+    	render: function() {
+    		var dogs = octopus.getDogs();
+    		this.dogListElem.html('');
+    		for (var i = 0; i < dogs.length; i++) {
+    			var dog = dogs[i];
+    			var elem = $('<li />');
+    			elem.text(dog.name);
+
+    			elem.on('click', function(dog) {
+    				return function() {
+    					octopus.setCurrentDog(dog);
+    					dogView.render();
+    				}
+    			}(dog));
+
+    			this.dogListElem.append(elem);
+    		}
+    	}
+    };
+
+    // var adminView = {
+    // 	init: function() {
+
+    // 	},
+    // 	render: function() {
+
+    // 	}
+    // }
+
+    octopus.init();
 });
-
-	
-
-	// var toggleDogView = function() {
-	$('.dog-name').on('click', function() {
-			// $clickContainer.appendTo($toggleWrapper);
-		$clickText.appendTo($clickContainer);
-		$clickCounterContainer.appendTo($clickContainer);
-		$dogView.appendTo('.dog-view-group');
-		$dogName.text(dog.name).appendTo($dogView);
-		$dogImage.attr('src', dog.imgPath).appendTo($dogView);
-		$clickContainer.appendTo($dogView);
-	});
-	});
-
-	// toggleDogView();
-
-    var increment = function() {
-    	var $clickTextEl = $(this).next().find(".click-text");
-        $clickTextEl.text(function(i, oldval) {
-            return ++oldval;
-        });
-    }
-
-    $(".dog").on('click', increment);
-});
-
-
-// build the click counters
